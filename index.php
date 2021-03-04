3e421c61d1b88c913180809ed5b0a38f43e1a92c
KRUG3R <luizfilipekruger@gmail.com>
2021-03-04 17:58:32 -0300

    ajuste de tamanho

--- index.php
+++ index.php
@@ -8,14 +8,14 @@
     <tr>
         <td><img src=https://www.isabelaflores.com/media/catalog/product/b/a/baixa-183-1.jpg></td>
         <td><img src=https://www.isabelaflores.com/media/catalog/product/b/a/baixa-183-3.jpg></td>
-        <td><img src=https://www.isabelaflores.com/media/catalog/product/b/a/baixa-183-2.jpg></td>
     </tr>
     <tr>
-        <td><img src=https://www.isabelaflores.com/media/catalog/product/b/a/baixa-297-1.jpg></td>
         <td><img src=https://www.isabelaflores.com/media/catalog/product/b/a/baixa-297-2.jpg></td>
         <td><img src=https://www.isabelaflores.com/media/catalog/product/b/a/baixa-297-3.jpg></td>
     </tr>
     </table>
+    <br>
+    <br>
     <marquee> <a href=https://api.whatsapp.com/send?phone=5511995850929&text=Ol%C3%A1.%20Tenho%20interesse%20na%20compra%20Flores> entre em contato pelo whatsapp</a> </marquee>
 </body>
 </html>
