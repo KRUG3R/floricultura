c2ba436c316d956c9f3a38998eeb450746a3227b
KRUG3R <luizfilipekruger@gmail.com>
2021-03-04 17:47:03 -0300

    incluido flores

--- index.php
+++ index.php
@@ -3,6 +3,18 @@
 <title> Floricultura </title>
 </head>
 <body>
-<h1> Floricultura boladona </h1>
+    <h1> Floricultura boladona </h1>
+    <table style="width:100%">
+    <tr>
+        <td><img src=https://www.isabelaflores.com/media/catalog/product/b/a/baixa-183-1.jpg></td>
+        <td><img src=https://www.isabelaflores.com/media/catalog/product/b/a/baixa-183-3.jpg></td>
+        <td><img src=https://www.isabelaflores.com/media/catalog/product/b/a/baixa-183-2.jpg></td>
+    </tr>
+    <tr>
+        <td><img src=https://www.isabelaflores.com/media/catalog/product/b/a/baixa-297-1.jpg></td>
+        <td><img src=https://www.isabelaflores.com/media/catalog/product/b/a/baixa-297-2.jpg></td>
+        <td><img src=https://www.isabelaflores.com/media/catalog/product/b/a/baixa-297-3.jpg></td>
+    </tr>
+    </table>
 </body>
 </html>
